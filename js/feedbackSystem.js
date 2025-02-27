c4bc9377beb990ad427a09933c3d18de4339b221
Maxine Calderon <mauxine2002@gmail.com>
2025-02-27 11:51:29 -0600

    Made feedback system

--- js/feedbackSystem.js
+++ js/feedbackSystem.js
@@ -1,7 +1,9 @@
-class FeedbackSystem {
+export class FeedbackSystem {
     static displayFeedback(isCorrect) {
       const feedbackSection = document.getElementById('feedback-section');
       feedbackSection.textContent = isCorrect ? 'Correct!' : 'Incorrect!';
       feedbackSection.style.color = isCorrect ? 'green' : 'red';
     }
   }
+
+window.FeedbackSystem = FeedbackSystem;
